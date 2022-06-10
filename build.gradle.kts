e3dc6aa5bc70620bd8ed872947793457c9218860
NickAc <32451103+NickAcPT@users.noreply.github.com>
2022-06-11 00:23:27 +0100

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 group = "io.github.orioncraftmc"
 base { archivesName.set("orion-launcher") }
-version = "0.0.1-SNAPSHOT"
+version = "0.0.2-SNAPSHOT"
 
 repositories {
     mavenCentral()
