6824c24300ed31b84decd04782ce7052e2b69dc5
NickAc <32451103+NickAcPT@users.noreply.github.com>
2022-06-10 22:20:47 +0100

    Maven deploy stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,41 @@
 plugins {
     java
+    `maven-publish`
 }
 
 group = "io.github.orioncraftmc"
+base { archivesName.set("orion-launcher") }
 version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
     maven("https://maven.fabricmc.net/")
     maven("https://raw.githubusercontent.com/OrionCraftMC/OrionMaven/main/")
-    flatDir {
-        dir("jars")
-    }
 }
 
 dependencies {
     implementation("info.picocli:picocli:4.6.3")
     implementation("io.github.orioncraftmc:sponge-mixin:0.0.1+mixin.0.8.5")
     implementation("net.fabricmc:mapping-io:0.3.0")
-
-    runtimeOnly(":original-obf")
-    runtimeOnly(":secret-agent")
 }
+
+publishing {
+    repositories {
+        val lightCraftRepoDir = project.findProperty("lightcraft.repo.location")
+        if (lightCraftRepoDir != null) {
+            maven {
+                name = "OrionCraftRepo"
+                url = File(lightCraftRepoDir.toString()).toURI()
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("mavenJava") {
+            this.artifactId = "orion-launcher"
+            from(components["java"])
+        }
+    }
+}
+
 
