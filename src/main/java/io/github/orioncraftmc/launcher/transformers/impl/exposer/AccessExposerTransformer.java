4a8e16709800d936f36b3926401539419447e0f3
NickAc <32451103+NickAcPT@users.noreply.github.com>
2022-06-11 22:35:00 +0100

    Only expose classes that are not annotated with (@)Mixin

--- src/main/java/io/github/orioncraftmc/launcher/transformers/impl/exposer/AccessExposerTransformer.java
+++ src/main/java/io/github/orioncraftmc/launcher/transformers/impl/exposer/AccessExposerTransformer.java
@@ -4,6 +4,9 @@ import io.github.orioncraftmc.launcher.transformers.OrionClassTransformer;
 import org.jetbrains.annotations.Nullable;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.util.Annotations;
 
 public class AccessExposerTransformer implements OrionClassTransformer {
     @Override
@@ -14,7 +17,16 @@ public class AccessExposerTransformer implements OrionClassTransformer {
     @Override
     public byte[] transformClass(String className, byte @Nullable [] classBytes) {
         if (classBytes == null) return null;
+
         ClassReader cr = new ClassReader(classBytes);
+        ClassNode cn = new ClassNode();
+        cr.accept(cn, 0);
+
+        // If the class is annotated with @Mixin, we need to ignore it.
+        if (Annotations.getInvisible(cn, Mixin.class) != null) {
+            return classBytes;
+        }
+
         ClassWriter cw = new ClassWriter(cr, 0);
         cr.accept(new AccessExposerClassVisitor(cw), 0);
 
